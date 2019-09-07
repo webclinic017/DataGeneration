148fb8934548e99b1477fb66e2c363c3f47b9f7c
taowenyin <wenyin.tao@163.com>
2019-09-07 12:18:34 +0800

    添加文件的自动删除

--- generation.py
+++ generation.py
@@ -156,6 +156,10 @@ if __name__ == '__main__':
 
     if not os.path.exists(DIST_FOLDER):
         os.mkdir(DIST_FOLDER)
+    else:
+        file_list = os.listdir(DIST_FOLDER)
+        for file in file_list:
+            os.remove(DIST_FOLDER + '/' + file)
 
     # 判断机型文件是否存在
     if not os.path.exists(MODEL_FILE):
